f405669c392d79e018201e31e7e6afd14ab64bff
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-13 04:40:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -403,7 +403,7 @@ async def leave(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.leave_chat(update.message.chat.id)
 
 # ======================= 🚀 اجرای نهایی =======================
-if name == "main":
+if __name__ == "__main__":
     print("🤖 خنگول فارسی 8.5.1 Cloud+ Supreme Pro Stable+ آماده به خدمت است ...")
 
     app = ApplicationBuilder().token(TOKEN).build()
