c3502799f0d96795f20b6b6ecb09fc5f308bfe71
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-20 06:37:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1556,25 +1556,28 @@ async def panel_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "panel_features": ("features.txt", "🧩 قابلیت‌های ربات"),
     }
 
-    if query.data in panels:
-        file_name, title = panels[query.data]
-        text = await load_text(file_name, f"❗ هنوز {title} ثبت نشده!")
-        text += "\n\n🔙 برای بازگشت، روی دکمه زیر بزن:"
-        back_btn = [[InlineKeyboardButton("🔙 بازگشت", callback_data="back_main")]]
-        await query.edit_message_text(text, reply_markup=InlineKeyboardMarkup(back_btn), parse_mode="HTML")
-    elif query.data == "panel_stats":
+  if query.data in panels:
+    file_name, title = panels[query.data]
+    text = await load_text(file_name, f"❗ هنوز {title} ثبت نشده!")
+    text += "\n\n🔙 برای بازگشت، روی دکمه زیر بزن:"
+    back_btn = [[InlineKeyboardButton("🔙 بازگشت", callback_data="back_main")]]
+    await query.edit_message_text(text, reply_markup=InlineKeyboardMarkup(back_btn), parse_mode="HTML")
+
+elif query.data == "panel_stats":
     user = query.from_user
     now = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
 
-    elif query.data == "panel_weather":
-        await show_weather(update, context)
-    elif query.data == "panel_fortune":
-        await query.message.reply_text("🔮 برای دیدن فال بنویس:\n<b>فال</b>", parse_mode="HTML")
-    elif query.data == "panel_joke":
-        await query.message.reply_text("😂 برای دیدن جوک بنویس:\n<b>جوک</b>", parse_mode="HTML")
-    elif query.data == "panel_font":
-        await query.message.reply_text("🎨 برای ساخت فونت بنویس:\n<b>فونت اسم‌ت</b>", parse_mode="HTML")
-    
+elif query.data == "panel_weather":
+    await show_weather(update, context)
+
+elif query.data == "panel_fortune":
+    await query.message.reply_text("🔮 برای دیدن فال بنویس:\n<b>فال</b>", parse_mode="HTML")
+
+elif query.data == "panel_joke":
+    await query.message.reply_text("😂 برای دیدن جوک بنویس:\n<b>جوک</b>", parse_mode="HTML")
+
+elif query.data == "panel_font":
+    await query.message.reply_text("🎨 برای ساخت فونت بنویس:\n<b>فونت اسم‌ت</b>", parse_mode="HTML")
         text = (
             f"📊 <b>اطلاعات کاربر:</b>\n\n"
             f"👤 نام: <b>{user.first_name}</b>\n"
