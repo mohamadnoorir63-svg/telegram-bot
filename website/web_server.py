34f2ba89c9d2a4840e6dabbfd63cd6cb75566346
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-22 00:40:20 +0200

    Update web_server.py

--- website/web_server.py
+++ website/web_server.py
@@ -1,25 +1,12 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route("/")
 def home():
-    return render_template_string("""
-    <html>
-      <head>
-        <title>ربات خنگول 🤖</title>
-        <meta charset="utf-8"/>
-      </head>
-      <body style="text-align:center; font-family:sans-serif;">
-        <h1>👋 سلام! این سایت رسمی ربات خنگول است</h1>
-        <p>ساخته شده توسط <b>@NOORI_NOOR</b></p>
-        <p>برای چت با ربات به 
-        <a href="https://t.me/Khenqol_bot">@Khenqol_bot</a> بروید.</p>
-      </body>
-    </html>
-    """)
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
