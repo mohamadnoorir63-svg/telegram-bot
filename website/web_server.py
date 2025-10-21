a1d5a8609af2c857f6a7330c954f0c9e59378a9b
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-21 17:54:49 +0200

    Update web_server.py

--- website/web_server.py
+++ website/web_server.py
@@ -1,18 +1,10 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=".", static_url_path="")
 
 @app.route('/')
-def index():
+def home():
     return send_from_directory('.', 'index.html')
 
-@app.route('/style.css')
-def css():
-    return send_from_directory('.', 'style.css')
-
-@app.route('/robots.txt')
-def robots():
-    return "User-agent: *\nAllow: /", 200, {'Content-Type': 'text/plain; charset=utf-8'}
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
