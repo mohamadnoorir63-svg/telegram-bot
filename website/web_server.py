9eb2125694c98ca3530e64cf69361f4bf30e9d69
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-21 17:31:55 +0200

    Update web_server.py

--- website/web_server.py
+++ website/web_server.py
@@ -1,1 +1,18 @@
+from flask import Flask, send_from_directory
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/style.css')
+def css():
+    return send_from_directory('.', 'style.css')
+
+@app.route('/robots.txt')
+def robots():
+    return "User-agent: *\nAllow: /", 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
