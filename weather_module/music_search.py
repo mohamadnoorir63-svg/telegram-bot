bcf8c793d7b997d983a64c8018295e685aeb09d2
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-24 16:07:26 +0200

    Update music_search.py

--- weather_module/music_search.py
+++ weather_module/music_search.py
@@ -1,1 +1,16 @@
+import yt_dlp
+import random
 
+async def search_music(query):
+    try:
+        ydl_opts = {"quiet": True, "format": "bestaudio/best"}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(f"ytsearch1:{query}", download=False)["entries"][0]
+            return {
+                "title": info["title"],
+                "url": info["webpage_url"],
+                "duration": info.get("duration", 0)
+            }
+    except Exception as e:
+        print("Search error:", e)
+        return None
