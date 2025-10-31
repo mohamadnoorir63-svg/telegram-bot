31585ac6613a8262795018ffe7d88b868fd521eb
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-10-31 16:55:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ from memory_manager import (
     generate_sentence,
     list_phrases
 )
-from jokes_manager import save_joke, list_jokes
+from jokes_manager import save_joke, delete_joke, list_jokes, send_random_joke
 from fortune_manager import save_fortune, list_fortunes, send_random_fortune, delete_fortune
 from group_manager import register_group_activity, get_group_stats
 from ai_learning import auto_learn_from_text
