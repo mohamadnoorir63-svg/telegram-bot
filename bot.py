7006bd85a81c3cc911e894d14baa13fc7512e922
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-11-14 21:03:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from telegram.ext import (
 import aiofiles
 
 # 📦 ماژول‌ها
-from font_maker import font_maker, next_font, prev_font, send_selected_font, feature_back
+from font_maker import font_maker, receive_font_name, next_font, prev_font, send_selected_font, feature_back, ASK_NAME
 from memory_manager import (
     init_files,
     load_data,
@@ -2314,7 +2314,6 @@ application.add_handler(CommandHandler("reply", toggle_reply_mode))
 # 🎨 فونت‌ساز خنگول
 # ==========================================================
 from telegram.ext import ConversationHandler
-from font_maker import font_maker, receive_font_name, next_font, prev_font, ASK_NAME
 font_handler = ConversationHandler(
     entry_points=[MessageHandler(filters.TEXT & filters.Regex(r"^فونت"), font_maker)],
     states={ASK_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_font_name)]},
