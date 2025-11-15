d52b39037e39be22490ac4a16eaa83b019133c1f
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-11-15 18:59:26 +0100

    Update group_lock.py

--- group_control/group_lock.py
+++ group_control/group_lock.py
@@ -1,42 +1,37 @@
-# group_control/group_lock.py
-import asyncio
-from telegram import Update
-from telegram.ext import MessageHandler, filters, ContextTypes
-# قفل‌کردن گروه
+from telegram import ChatPermissions, Update
+from telegram.ext import CommandHandler, Application, ContextTypes  # <--- اضافه کردن Application
+
+# قفل کردن گروه
 async def lock_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.effective_chat.type.endswith("group"):
         return await update.message.reply_text("این دستور فقط در گروه کار می‌کند.")
-
     try:
         await update.effective_chat.set_permissions(
-            ChatPermissions(     # هیچ‌کس نتواند پیام بدهد
+            ChatPermissions(
                 can_send_messages=False,
                 can_send_media_messages=False,
                 can_send_other_messages=False,
                 can_add_web_page_previews=False
             )
         )
-        await update.message.reply_text("🔒 گروه *قفل* شد.\nاعضا اجازه ارسال پیام ندارند.", parse_mode="Markdown")
-
+        await update.message.reply_text("🔒 گروه *قفل* شد.", parse_mode="Markdown")
     except Exception as e:
         await update.message.reply_text(f"خطا: {e}")
 
-# بازکردن گروه
+# باز کردن گروه
 async def unlock_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.effective_chat.type.endswith("group"):
         return await update.message.reply_text("این دستور فقط در گروه کار می‌کند.")
-
     try:
         await update.effective_chat.set_permissions(
-            ChatPermissions(     # اجازه کامل
+            ChatPermissions(
                 can_send_messages=True,
                 can_send_media_messages=True,
                 can_send_other_messages=True,
                 can_add_web_page_previews=True
             )
         )
-        await update.message.reply_text("🔓 گروه *باز* شد.\nاعضا می‌توانند پیام ارسال کنند.", parse_mode="Markdown")
-
+        await update.message.reply_text("🔓 گروه *باز* شد.", parse_mode="Markdown")
     except Exception as e:
         await update.message.reply_text(f"خطا: {e}")
 
