6fc05e1c7c79a1bf802ca34e26753bcc9764f477
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-11-19 00:14:23 +0100

    Delete farsi_commands directory

--- farsi_commands/init.py
+++
@@ -1,5 +0,0 @@
-from .speaker import register_speaker_commands
-
-def register_all_farsi(app):
-    """ثبت همه دستورات فارسی"""
-    register_speaker_commands(app)
--- farsi_commands/speaker.py
+++
@@ -1,7 +0,0 @@
-from telegram.ext import CommandHandler
-from bot import mute_speaker, unmute_speaker  # یا مسیر دقیق تو
-
-def register_speaker_commands(application):
-    """ثبت دستورات سخنگوی فارسی"""
-    application.add_handler(CommandHandler("سخنگو_روشن", unmute_speaker))
-    application.add_handler(CommandHandler("سخنگو_خاموش", mute_speaker))
