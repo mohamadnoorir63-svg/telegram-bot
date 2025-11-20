1cad9c34700758ac90b36ad0f9062d440a6c8526
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-11-20 02:15:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,14 @@ from welcome_module import (
     welcome
 )
 from jokes_manager import save_joke, delete_joke, list_jokes, send_random_joke
-from fortune_manager import save_fortune, list_fortunes, send_random_fortune, delete_fortune
+from fortune_manager import (
+    save_fortune,
+    list_fortunes,
+    send_random_fortune,
+    delete_fortune,
+    is_admin_or_sudo,
+    is_sudo
+)
 from group_manager import register_group_activity, get_group_stats
 from ai_learning import auto_learn_from_text
 from smart_reply import detect_emotion, smart_response
