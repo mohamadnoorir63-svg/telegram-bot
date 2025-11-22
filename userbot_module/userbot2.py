6f5e155590bc5e13250b9caef6bea3f1de7d2484
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-11-22 21:09:15 +0100

    Update userbot2.py

--- userbot_module/userbot2.py
+++ userbot_module/userbot2.py
@@ -1,1 +1,12 @@
+from telethon import TelegramClient
 
+API_ID = 123456
+API_HASH = "your_api_hash"
+SESSION = "userbot2"
+
+client2 = TelegramClient(SESSION, API_ID, API_HASH)
+
+async def start_userbot2():
+    print("⚡ یوزربات دوم در حال اجراست...")
+    await client2.start()
+    await client2.run_until_disconnected()
