2e1ec5a73b2e0afe2cf5ddb7631a1ae07ded1284
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-11-24 13:07:21 +0100

    Update userbot2.py

--- userbot_module/userbot2.py
+++ userbot_module/userbot2.py
@@ -35,7 +35,11 @@ from telethon.errors import (
 # ============================
 # ======= CONFIG ============
 # ============================
+API_ID = 32796779
+API_HASH = "4deabef1568103b3242db6f74a73e8a5"
+SESSION_STRING = "1ApWapzMBuzET2YvEj_TeHnWFPVKUV1Wbqb3o534-WL_U0fbXd-RTUWuML8pK60sh9B_oGsE3T3RQjIhXWs4tM30UPr3BFxpF6EUCB9BSPGCtmienHmXHI9k-zT7iI6HZLtqlNeGi0zMxAA8hUY25V1IhKgnujyHWcUA9VfVXNmJTtq54cZgdvTSa3EntYNmTlMcsaX7p82yoSKpz3LL5SB9ZL35PZCVAVXMIcfBbv_Ofr6w9CA4yBcMm9-t4NjRRLaZnwH-rU29RmtM8qM3n-K7mvCFRfQ1Vmw_HBFcYJlx-mHN_rxgo55XIC3Y3_9XoQ9f0FypxXgxEsYUjH5LosGP2KA_tMZo="
 
+client2 = TelegramClient(StringSession(SESSION
 
 # سودوها (ID عددی)
 SUDO_USERS = [8588347189]
