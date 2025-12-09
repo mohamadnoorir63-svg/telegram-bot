1248cb9c4dca31eabbc6d0a3beffb344643be299
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-12-09 21:59:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1534,27 +1534,18 @@ application.add_handler(
     CallbackQueryHandler(tiktok_audio_handler, pattern=r"^tiktok_audio:"),
     group=-1000
 )
+
 from modules.soundcloud_handler import (
     soundcloud_handler,
     music_select_handler,
-    inline_sc
-)
-from telegram.ext import MessageHandler, CallbackQueryHandler, InlineQueryHandler, filters
-
-application.add_handler(
-    MessageHandler(filters.TEXT & ~filters.COMMAND, soundcloud_handler),
-    group=-2000
+    inline_sc,
+    music_inline_handler
 )
 
-application.add_handler(
-    CallbackQueryHandler(music_select_handler, pattern=r"^music_select:"),
-    group=-2000
-)
-
-application.add_handler(
-    InlineQueryHandler(inline_sc),
-    group=-3000
-)
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, soundcloud_handler), group=-2000)
+application.add_handler(CallbackQueryHandler(music_select_handler, pattern=r"^music_select:"), group=-2000)
+application.add_handler(InlineQueryHandler(inline_sc), group=-3000)
+application.add_handler(CallbackQueryHandler(music_inline_handler, pattern=r"^music_inline:"), group=-2000)
 from modules.instagram_downloader import instagram_handler
 
 application.add_handler(
