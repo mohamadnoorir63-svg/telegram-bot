2e89be7b3d6a7d9467888936b76900c70a369cd3
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-12-09 23:58:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1535,14 +1535,21 @@ application.add_handler(
     CallbackQueryHandler(tiktok_audio_handler, pattern=r"^tiktok_audio:"),
     group=-1000
 )
+
 from modules.soundcloud_handler import soundcloud_handler, music_select_handler
 from telegram.ext import MessageHandler, CallbackQueryHandler, filters
 
+# ----------------------
+# هندلر پیام‌های متنی
+# ----------------------
 application.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, soundcloud_handler),
     group=-2000
 )
 
+# ----------------------
+# هندلر دکمه‌های انتخاب موزیک
+# ----------------------
 application.add_handler(
     CallbackQueryHandler(music_select_handler, pattern=r"^music_select:"),
     group=-2000
