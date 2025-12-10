0c06dd839dbd4275cbeb1f6659b91da6a093bd71
mohamadnoorir63-svg <mohamadnoorir63@gmail.com>
2025-12-10 03:15:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1535,27 +1535,21 @@ application.add_handler(
     CallbackQueryHandler(tiktok_audio_handler, pattern=r"^tiktok_audio:"),
     group=-1000
 )
-from modules.soundcloud_handler import (
-    soundcloud_handler,
-    music_select_handler,
-    inline_sc
-)
-from telegram.ext import MessageHandler, CallbackQueryHandler, InlineQueryHandler, filters
 
+from modules.soundcloud_handler import soundcloud_handler, music_select_handler
+from telegram.ext import MessageHandler, CallbackQueryHandler, filters
+
+# همهٔ پیام‌های متنی (غیر از کامند) به soundcloud_handler ارسال می‌شوند
 application.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, soundcloud_handler),
     group=-2000,
 )
 
+# CallbackQueryHandler برای انتخاب موزیک، با pattern هماهنگ "music_select:"
 application.add_handler(
     CallbackQueryHandler(music_select_handler, pattern=r"^music_select:"),
     group=-2000,
 )
-
-application.add_handler(
-    InlineQueryHandler(inline_sc),
-)
-
 from modules.instagram_downloader import instagram_handler
 
 application.add_handler(
